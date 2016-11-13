82ee69589bf4b2aed238e377de736dc72b5488d3
shawa <andrewshaw.mail@gmail.com>
2016-11-13 15:21:20 +0000

    nicer message definition

--- messages.js
+++ messages.js
@@ -1,39 +1,55 @@
-function message(pattern, sideEffects, responseTemplate) {
+const format = require('string-format');
+const chatty = require('./chatty');
+
+format.extend(String.prototype);
+
+const message = (pattern, sideEffects, responseTemplate) => {
   return {
-    pattern: pattern,
+    matcher: new RegExp(pattern),
     sideEffects: sideEffects,
     responseTemplate: responseTemplate,
   };
 }
 
 
-function exec(input, message) {
-  const matcher = new RegExp(message.pattern);
-  const captures = input.match(matcher);
+function execute(input, message) {
+  const matched = input.match(message.matcher);
+
+  if (!matched) {
+    return false;
+  }
+
+  const captures = matched.slice(1);
   const returnValues = message.sideEffects(...captures);
   const response = message.responseTemplate.format(returnValues);
-
   return response;
-};
+}
 
-const CLIENT_JOIN = message(
+const msgs = {};
+
+msgs.CLIENT_JOIN = message(
   'JOIN_CHATROOM: (.+)\n' +
   'CLIENT_IP: 0\n' +
   'PORT: 0\n' +
-  'CLIENT_NAME: (.+)\n',
+  'CLIENT_NAME: (.+)',
 
   (roomName, clientName) => {
-    rooms[roomName].push(clientName);
-    let joinId = getJoinId();
+    console.log(clientName);
     return {
       roomName: roomName,
-      roomRef: getRoomRef(roomName),
-      joinId: getJoinId(),
+      roomRef: chatty.getRoomRef(roomName),
+      joinId: chatty.getJoinId(),
     };
   },
 
-  'JOINED_CHATROOM: {{roomName}}\n' +
+  'JOINED_CHATROOM: {roomName}\n' +
   'SERVER_IP: 0\n' +
   'PORT: 0\n' +
-  'ROOM_REF: {{roomRef}}\n' +
-  'JOIN_ID: {{joinID}}\n');
+  'ROOM_REF: {roomRef}\n' +
+  'JOIN_ID: {joinId}\n');
+
+
+module.exports = {
+  execute: execute,
+  msgs: msgs,
+};
