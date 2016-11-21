b6d6016f40db98aa12cd56abd89ee167f7a1db4e
shawa <andrewshaw.mail@gmail.com>
2016-11-21 20:13:57 +0000

    Add the ip hardcoded

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ set -euo pipefail; IFS=$'\n\t'
 
 export SK_PORTNUMBER="$1"
 export SK_STUDENTID=13323657
-export SK_IP_ADDRESS=$(/sbin/ifconfig en0 inet \
-                     | grep -o "\d\+\.\d\+\.\d\+\.\d\+" \
-                     | head -n 1 \
-                     | tr -d '\n')
+#export SK_IP_ADDRESS=$(/sbin/ifconfig en0 inet \
+#                     | grep -o "\d\+\.\d\+\.\d\+\.\d\+" \
+#                     | head -n 1 \
+#                     | tr -d '\n')
+export SK_IP_ADDRESS="10.62.0.10"
 mix clean
 mix run --no-halt
