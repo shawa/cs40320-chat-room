4b2dbba8b6fc9140b1f59484423d8b784527b817
shawa <andrewshaw.mail@gmail.com>
2016-11-21 19:58:15 +0000

    fix ip getting/

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -euo pipefail; IFS=$'\n\t'
 
 export SK_PORTNUMBER="$1"
 export SK_STUDENTID=13323657
-export SK_IP_ADDRESS=$(ifconfig en0 inet \
+export SK_IP_ADDRESS=$(/sbin/ifconfig en0 inet \
                      | grep -o "\d\+\.\d\+\.\d\+\.\d\+" \
                      | head -n 1 \
                      | tr -d '\n')
