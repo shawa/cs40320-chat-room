0865c8fbbdd6b90b2ffc6b176779ed7e6789cce2
shawa <andrewshaw.mail@gmail.com>
2016-11-21 20:49:16 +0000

    we overshot the newlines

--- lib/echo.ex
+++ lib/echo.ex
@@ -107,7 +107,6 @@ defmodule Echo do
     CLIENT_NAME:#{client_name}
     MESSAGE:#{client_name} joined ##{room_name}
 
-
     """ |> post_to(room_ref)
   end
 
@@ -151,7 +150,6 @@ defmodule Echo do
     CHAT:#{room_ref}
     CLIENT_NAME:#{client_name}
     MESSAGE:#{message}
-
 
     """ |> post_to(room_ref)
   end
