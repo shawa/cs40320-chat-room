5cf09d2d4ed4653e3cb1ce5fb063306d0880b127
shawa <andrewshaw.mail@gmail.com>
2016-11-24 12:45:02 +0000

    Add via tuple and per room message targeting

--- lib/chat/server.ex
+++ lib/chat/server.ex
@@ -3,24 +3,27 @@ defmodule Chat.Server do
 
   # API
   def start_link(name) do
-    GenServer.start_link(__MODULE__, [], name: :chat_room)
+    GenServer.start_link(__MODULE__, [], name: via_tuple(name)
   end
 
-  def add_message(message) do
-    GenServer.cast(:chat_room, {:add_message, message})
+  def add_message(room_name, message) do
+    GenServer.cast(via_tuple(room_name), {:add_message, message})
   end
 
-  def get_messages do
-    GenServer.call(:chat_room, :get_messages)
+  def get_messages(room_name) do
+    GenServer.call(via_tuple(room_name), :get_messages)
   end
 
+  defp via_tuple(room_name) do
+    {:via, Chat.Registry, {:chat_room, room_name}}
+  end
   # SERVER
   def init(messages) do
     {:ok, messages}
   end
 
   def handle_cast({:add_message, new_message}, messages) do
-    {:noreply, [new_message |messages]}
+    {:noreply, [new_message | messages]}
   end
 
   def handle_call(:get_messages, _from, messages) do
