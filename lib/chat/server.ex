5066c42967f5d88b5d91c07c1ae99d4a42aa5ef6
shawa <andrewshaw.mail@gmail.com>
2016-11-24 17:36:12 +0000

    add members in list

--- lib/chat/server.ex
+++ lib/chat/server.ex
@@ -14,21 +14,43 @@ defmodule Chat.Server do
     GenServer.call(via_tuple(room_name), :get_messages)
   end
 
+  def add_member(client_name) do
+    GenServer.cast(via_tuple(room_name), {:add_message, client_name})
+  end
+
+  def drop_member(client_name) do
+    GenServer.cast(via_tuple(room_name), {:drop_member, client_name})
+  end
+
+
   defp via_tuple(room_name) do
     {:via, Chat.Registry, {:chat_room, room_name}}
   end
 
   # SERVER
 
-  def init(messages) do
-    {:ok, messages}
+  def init(messages, members) do
+    {:ok, messages, members}
   end
 
   def handle_cast({:add_message, new_message}, messages) do
     {:noreply, [new_message | messages]}
   end
 
+  def handle_cast({:add_member, new_member}, members) do
+    {:noreply, [new_member | members}
+  end
+
+  def handle_cast({:drop_member, member_to_drop}, members) do
+    {:noreply, List.delete(members, member_to_drop)}
+  end
+
   def handle_call(:get_messages, _from, messages) do
     {:reply, messages, messages}
   end
+
+  def handle_call(:get_members, _from, members) do
+    {:reply, members, members}
+  end
+
 end
