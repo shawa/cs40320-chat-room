e6c346b0e870546aadc327241c05af82b1ec795e
shawa <andrewshaw.mail@gmail.com>
2016-11-24 19:50:27 +0000

    Add join ids

--- lib/chat/server.ex
+++ lib/chat/server.ex
@@ -1,4 +1,5 @@
 defmodule Chat.Server do
+  require Logger
   use GenServer
 
   # API
@@ -6,19 +7,19 @@ defmodule Chat.Server do
     GenServer.start_link(__MODULE__, [], name: via_tuple(name))
   end
 
-  def add_message(room_name, message) do
+  def add_message(message, room_name) do
     GenServer.cast(via_tuple(room_name), {:add_message, message})
   end
 
-  def get_messages(room_name) do
-    GenServer.call(via_tuple(room_name), :get_messages)
+  def add_member(client_name, room_name) do
+    GenServer.call(via_tuple(room_name), {:add_member, client_name})
   end
 
-  def add_member(client_name) do
-    GenServer.cast(via_tuple(room_name), {:add_message, client_name})
+  def get_members(room_name) do
+    GenServer.call(via_tuple(room_name), :get_members)
   end
 
-  def drop_member(client_name) do
+  def drop_member(client_name, room_name) do
     GenServer.cast(via_tuple(room_name), {:drop_member, client_name})
   end
 
@@ -29,20 +30,22 @@ defmodule Chat.Server do
 
   # SERVER
 
-  def init(messages, members) do
-    {:ok, messages, members}
+  def init(state) do
+    {:ok, state}
   end
 
-  def handle_cast({:add_message, new_message}, messages) do
-    {:noreply, [new_message | messages]}
-  end
-
-  def handle_cast({:add_member, new_member}, members) do
-    {:noreply, [new_member | members}
+  def handle_cast({:add_message, message}, state) do
+    Logger.info "Broadcasting #{message}"
+    {:noreply, state}
   end
 
   def handle_cast({:drop_member, member_to_drop}, members) do
     {:noreply, List.delete(members, member_to_drop)}
+  end
+
+  def handle_call({:add_member, new_member}, _from,  members) do
+    join_id = :erlang.unique_integer
+    {:reply, {:ok, join_id}, [new_member | members]}
   end
 
   def handle_call(:get_messages, _from, messages) do
